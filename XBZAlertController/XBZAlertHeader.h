13958a809cd5ff9f65943798ad12f2d142b89d08
BigKing <641648019@qq.com>
2019-01-18 13:30:46 +0800

    update

---
+++ XBZAlertController/XBZAlertHeader.h
@@ -0,0 +1,14 @@
+//
+//  XBZAlertHeader.h
+//  XBZAlertController
+//
+//  Created by BigKing on 2019/1/18.
+//  Copyright © 2019 BigKing. All rights reserved.
+//
+
+#ifndef XBZAlertHeader_h
+#define XBZAlertHeader_h
+
+#import "UIAlertController+XBZAlertController.h"
+
+#endif /* XBZAlertHeader_h */
